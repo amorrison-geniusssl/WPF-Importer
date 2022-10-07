fcf195ca9bb302d0de5a2ab5499e5675bf119365
Alan Morrison <AMorrison@geniusssl.com>
2022-10-07 12:25:17 +0100

    Implemmented ValidationViewModelBase

---
+++ ImporterUI/ViewModels/ValidationViewModelBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImporterUI.ViewModels
+{
+    public class ValidationViewModelBase : ViewModelBase, INotifyDataErrorInfo
+    {
+        public bool HasErrors => throw new NotImplementedException();
+
+        public event EventHandler<DataErrorsChangedEventArgs>? ErrorsChanged;
+
+        public IEnumerable GetErrors(string? propertyName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
